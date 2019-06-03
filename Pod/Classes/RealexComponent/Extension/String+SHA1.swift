ba73358579e9de2896cdd0525930dd577280a7ff
Miguel Berrocal Gómez <miguel@helm.cat>
2019-06-03 16:06:34 +0200

    Update to swift 5.

--- Pod/Classes/RealexComponent/Extension/String+SHA1.swift
+++ Pod/Classes/RealexComponent/Extension/String+SHA1.swift
@@ -9,10 +9,9 @@ extension String {
 
 	var sha1: String? {
 		guard let data = self.data(using: String.Encoding.utf8) else { return nil }
-
-		let hash = data.withUnsafeBytes { (bytes: UnsafePointer<Data>) -> [UInt8] in
+		let hash = data.withUnsafeBytes { (bytes) -> [UInt8] in
 			var hash: [UInt8] = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
-			CC_SHA1(bytes, CC_LONG(data.count), &hash)
+			CC_SHA1(bytes.baseAddress, CC_LONG(data.count), &hash)
 			return hash
 		}
 
