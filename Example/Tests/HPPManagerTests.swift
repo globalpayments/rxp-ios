2c5d560aba28f7ff0677a38dc0a90fcf940bdb8a
Alexandr Pavlov <alexandr.pavlov@outlook.com>
2020-05-13 16:19:27 +0300

    Add HPPManager tests

---
+++ Example/Tests/HPPManagerTests.swift
@@ -0,0 +1,86 @@
+//
+//  HPPManagerTests.swift
+//  RXPiOS_Tests
+//
+//  Copyright © 2020 realexpayments. All rights reserved.
+//
+
+import XCTest
+@testable import RXPiOS
+
+class HPPManagerTests: XCTestCase {
+
+    let session = URLSessionMock()
+    var sut: HPPManager?
+
+    override func setUp() {
+
+        sut = HPPManager(session: session)
+        sut?.HPPRequestProducerURL = URL(string: "https://www.example.com/HppRequestProducer")
+        sut?.HPPURL = URL(string: "https://pay.sandbox.realexpayments.com/pay")
+        sut?.HPPResponseConsumerURL = URL(string: "https://www.example.com/HppResponseConsumer")
+    }
+
+    override func tearDown() {
+        sut = nil
+    }
+
+    func testHPPManagerShouldNotContainUserAgentHeaderField() {
+        // Given
+        let viewController = UIViewController()
+        // When
+        sut?.presentViewInViewController(viewController)
+        // Then
+        let request: URLRequest? = session.request
+        XCTAssertFalse((request?.allHTTPHeaderFields?.keys.contains("User-Agent"))!)
+    }
+
+    func testHPPManagerShouldContainUserAgentHeaderField() {
+        // Given
+        let viewController = UIViewController()
+        sut?.enableUserAgent = true
+        // When
+        sut?.presentViewInViewController(viewController)
+        // Then
+        let request: URLRequest? = session.request
+        XCTAssertTrue((request?.allHTTPHeaderFields?.keys.contains("User-Agent"))!)
+    }
+
+    func testHPPManagerShouldNotContainAdditionalHeaders() {
+        // Given
+        let viewController = UIViewController()
+        // When
+        sut?.presentViewInViewController(viewController)
+        // Then
+        XCTAssertNil(sut?.additionalHeaders)
+    }
+
+    func testHPPManagerShouldContainAdditionalHeader() {
+        // Given
+        let viewController = UIViewController()
+        sut?.additionalHeaders = ["custom_header": "test_value"]
+        // When
+        sut?.presentViewInViewController(viewController)
+        // Then
+        let request: URLRequest? = session.request
+        XCTAssertNotNil(sut?.additionalHeaders)
+        XCTAssertTrue((request?.allHTTPHeaderFields?.keys.contains("custom_header"))!)
+        XCTAssertEqual(request?.value(forHTTPHeaderField: "custom_header"), "test_value")
+    }
+
+    func testHPPManagerShouldContainAdditionalHeaders() {
+        // Given
+        let viewController = UIViewController()
+        sut?.additionalHeaders = ["custom_header_1": "test_value_1",
+                                  "custom_header_2": "test_value_2"]
+        // When
+        sut?.presentViewInViewController(viewController)
+        // Then
+        let request: URLRequest? = session.request
+        XCTAssertNotNil(sut?.additionalHeaders)
+        XCTAssertTrue((request?.allHTTPHeaderFields?.keys.contains("custom_header_1"))!)
+        XCTAssertTrue((request?.allHTTPHeaderFields?.keys.contains("custom_header_2"))!)
+        XCTAssertEqual(request?.value(forHTTPHeaderField: "custom_header_1"), "test_value_1")
+        XCTAssertEqual(request?.value(forHTTPHeaderField: "custom_header_2"), "test_value_2")
+    }
+}
